a9367f511c7c72357b7ff64a57abd7ca3ae32aa3
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-02-25 18:59:32 +0600

    another component  added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var name = " Tanim";
+  const products = [
+    {name: 'Photoshop', price:'$99.99'},
+    {name: 'Illustator', price:'$70.99'},
+    {name: 'PDF Reader', price:'$9.99'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +17,9 @@ function App() {
       <Person name="Ashab Hussan" status="Muslim"></Person>
       <Person name="Sabir Hasan" status="Lawer"></Person>
 
-      
+      <Product name={products[0].name} price={products[0].price} ></Product>
+      <Product name={products[1].name} price={products[1].price} ></Product>
+      <Product name={products[2].name} price={products[2].price} ></Product>
 
 
 
@@ -32,6 +38,26 @@ function Person(props) {
     <div style={personStyle}>
       <h1>{props.name}</h1>
       <h3>{props.status}</h3>
+    </div>
+  );
+}
+
+function Product(props) {
+  const productStyle = {
+    border : '1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height: '250px',
+    width: '250px',
+    float: 'left',
+    margin: '10px'
+  }
+
+  return (
+    <div style={productStyle}>
+      <h2>{props.name} </h2>
+      <h1>{props.price}</h1>
+      <button>Buy Now</button>
     </div>
   );
 }
